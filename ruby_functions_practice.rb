8b73643724e0cccfa94b2b127280da16dbde8c11
Ethan Radcliffe <ethanr@mac.com>
2019-01-15 17:04:07 +0000

    Fix directory structure

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,46 @@
+def return_10
+  return 10
+end
+
+def add (first_number, second_number)
+  return first_number + second_number
+end
+
+def subtract (first_number, second_number)
+  return first_number - second_number
+end
+
+def multiply (first_number, second_number)
+  return first_number * second_number
+end
+
+def divide (first_number, second_number)
+  return first_number / second_number
+end
+
+def length_of_string (string)
+  return string.length
+end
+
+def join_string (string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number (string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
+
+def number_to_full_month_name (month_number)
+  if month_number == 1
+      return "January"
+
+    elsif month_number == 3
+      return "March"
+
+    else month_number == 9
+      return "September"
+      end
+end
+
+def substring_month (month_number)
+  if mon
--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -61,16 +61,16 @@ class FunctionsTest < MiniTest::Test
   end
 
 
-#   def test_number_to_full_name__month_9()
-#     result = number_to_full_month_name( 9 )
-#     assert_equal( "September", result )
-#   end
-#
-#   def test_substring__month_1()
-#     first_month_string = number_to_short_month_name( 1 )
-#     assert_equal( "Jan", first_month_string )
-#   end
-#
+  def test_number_to_full_name__month_9()
+    result = number_to_full_month_name( 9 )
+    assert_equal( "September", result )
+  end
+
+  def test_substring__month_1()
+    first_month_string = number_to_short_month_name( 1 )
+    assert_equal( "Jan", first_month_string )
+  end
+
 #   def test_substring__month_4()
 #     fourth_month_string = number_to_short_month_name( 4 )
 #     assert_equal( "Apr", fourth_month_string )
